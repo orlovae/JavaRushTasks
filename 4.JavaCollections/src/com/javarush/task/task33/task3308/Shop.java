7afbc5cd0c6a39c28ca472612605742484d6ecc6
Alexander Orlov <79202875363@ya.ru>
2019-08-22 12:57:13 +0300

    task3307 решена

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3308/Shop.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task33.task3308;
+
+import javax.xml.bind.annotation.XmlAnyElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import java.util.List;
+
+@XmlRootElement
+@XmlType(name = "shop")
+public class Shop {
+    public Goods goods;
+    public int count;
+    public double profit;
+    public String[] secretData;
+
+    @XmlType(name = "goods")
+    public static class Goods {
+        @XmlAnyElement
+        public List<String> names;
+    }
+}
