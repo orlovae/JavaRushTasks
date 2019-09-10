3450eb7003479f8c54d86badccc9c0d3d800c481
Alexander Orlov <79202875363@ya.ru>
2019-09-10 11:33:22 +0300

    task3402 решена

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3402/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task34.task3402;
+
+/* 
+Факториал с помощью рекурсии
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.factorial(9));     //362880
+        System.out.println(solution.factorial(0));     //1
+        System.out.println(solution.factorial(1));     //1
+    }
+
+    public int factorial(int n) {
+        int result = 1;
+        if (n == 1 || n == 0) {
+            return result;
+        }
+        result = n * factorial(n - 1);
+        return result;
+    }
+}
